6540b18a29299712254a29cb88242427947be45b
Mangb0 <whdudghks45629@gmail.com>
2024-03-08 01:04:33 +0900

    상세페이지 추가 및 css 수정

---
+++ src/components/GalleryDetail.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+
+const GalleryDetail: FC = () => {
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-header-wrapper">
+        <div className="header-section">
+          <div className="header-button"></div>
+        </div>
+        <div className="header-section">
+          <div className="header-button"></div>
+          <div className="header-button"></div>
+          <div className="header-button">
+            <span>다운로드</span>
+          </div>
+          <div className="header-button"></div>
+        </div>
+      </div>
+      <div className="modal-content">
+        <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
+        <div className="modal-next-button" style={{ left: "24px" }}>
+          {"<"}
+        </div>
+        <div className="modal-next-button" style={{ right: "24px" }}>
+          {">"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryDetail;
