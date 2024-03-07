850080b9c42d69c60f04c132b2dd3c4380498edf
Mangb0 <whdudghks45629@gmail.com>
2024-03-07 21:51:50 +0900

    동적 그리드 구현 및 이미지 데이터 예외처리

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,32 +1,54 @@
-import { FC } from "react";
+import { FC, SyntheticEvent, useEffect, useState } from "react";
+
+interface Image {
+  _id: string;
+}
 
 const Gallery: FC = () => {
+  const [images, setImages] = useState<Image[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // 데이터 로드
+  useEffect(() => {
+    fetch("./test.json")
+      .then((response) => {
+        if (!response.ok) console.log(response.ok);
+        console.log(response);
+        console.log(response.ok);
+        return response.json();
+      })
+      .then((data) => {
+        setImages(data.renderings);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("데이터 로드 중 오류가 발생하였습니다");
+      });
+  }, []);
+
+  // 이미지 예외 처리
+  const handleImageError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = "./imageError.jpg";
+  };
+
   return (
     <div className="gallery-wrapper">
       <div className="header-wrapper">
-        <div className="header_left">13 개의 렌더샷</div>
+        <div className="header_left">{images.length} 개의 렌더샷</div>
         <div className="header-middle">갤러리</div>
         <div className="header-right"></div>
       </div>
       <div className="card-wrapper">
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
-        <div className="card-item">
-          <img src="https://resources.archisketch.com/images/Xx9q9qjFDF46D26A4A54BA5/550xAUTO/Xx9q9qjFDF46D26A4A54BA5.png" />
-        </div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          images.map((image, i) => (
+            <div key={i} className="card-item">
+              <img src={image._id} onError={handleImageError} />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
